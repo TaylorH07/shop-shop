fd63181816e29b94eb86c1adbd97e32f6372b890
taylor.holmes <taylorh2343@gmail.com>
2022-04-15 21:23:27 -0500

    completed module

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,9 +5,17 @@ import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import "./style.css";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
 
   useEffect(() => {
     async function getCart() {
@@ -30,6 +38,20 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
   }
 
   if (!state.cartOpen) {
@@ -73,6 +95,10 @@ const Cart = () => {
           You haven't added anything to your cart yet!
           </h3>
         )}
+
+      <button onClick={submitCheckout}>
+        Checkout
+      </button>
     </div>
   );
 };
